a7003caadaaf0bbfada1dba259345e686003014e
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-03-12 03:28:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee1.ci9cl96hqxwx.ap-south-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "bloodreaper"
-customdb = "employee1"
+customdb = "employee"
 custombucket = "empbuk"
 customregion = "ap-south-1"
